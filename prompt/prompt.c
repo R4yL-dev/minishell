86dd7f2110d43a0f3b980ae71b42d7200588a1ed
Luca Ray <luca.ray@protonmail.ch>
2023-08-06 19:55:38 +0200

    add basic prompt

---
+++ prompt/prompt.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prompt.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/06 19:19:00 by lray              #+#    #+#             */
+/*   Updated: 2023/08/06 19:50:10 by lray             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+char	*prompt_get(void)
+{
+	char	*input;
+
+	input = readline("minishell> ");
+	if (input == NULL || ft_strlen(input) == 0)
+	{
+		free(input);
+		return (NULL);
+	}
+	add_history(input);
+	return (input);
+}
---
+++ prompt/prompt.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prompt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/06 19:19:26 by lray              #+#    #+#             */
+/*   Updated: 2023/08/06 19:28:45 by lray             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PROMPT_H
+# define PROMPT_H
+
+# include <readline/readline.h>
+# include <readline/history.h>
+
+char	*prompt_get(void);
+#endif
