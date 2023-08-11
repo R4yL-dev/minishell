27a2dc6509b7cbec0abd46550df6a9d5864ea70d
Luca Ray <luca.ray@protonmail.ch>
2023-08-11 23:31:18 +0200

    delete tklist

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/06 19:13:07 by lray              #+#    #+#             */
-/*   Updated: 2023/08/11 16:30:13 by lray             ###   ########.fr       */
+/*   Updated: 2023/08/11 23:28:59 by lray             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 
 # include <stdio.h>
 
-# include "lexer/tklist.h"
+# include "lexer/token.h"
+# include "lexer/dyntklist.h"
 # include "parser/tree.h"
+# include "exec/dynarrstr.h"
 
 # include "libft/libft.h"
 # include "prompt/prompt.h"
 # include "lexer/lexer.h"
 # include "parser/parser.h"
 # include "exec/exec.h"
-# include "exec/dynarrstr.h"
 
 void	free_split(char **splitted_arr);
 
-void	db_show_tklist(t_tklist *tklist);
 void	db_show_tree(t_tree_node *node, int depth);
 
 #endif
