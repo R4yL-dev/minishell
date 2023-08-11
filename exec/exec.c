80a5b3cd5e5b5ea9693e3075d220448ff2569173
Luca Ray <luca.ray@protonmail.ch>
2023-08-11 19:41:21 +0200

    update exec to use new dynarrstr

--- exec/exec.c
+++ exec/exec.c
@@ -6,26 +6,34 @@
 /*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/06 22:19:41 by lray              #+#    #+#             */
-/*   Updated: 2023/08/11 02:43:59 by lray             ###   ########.fr       */
+/*   Updated: 2023/08/11 19:39:57 by lray             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static char	**tree_to_argv(t_tree_node *tree);
 static int	is_cmd(char *path);
 
-void exec(t_tree_node *tree, char **envp)
+void exec(t_tree_node *tree)
 {
-	char	**argv;
+	t_dynarrstr	*dynarr;
 	pid_t	pid;
 	char	*tmp;
+	int		i;
 
-	argv = tree_to_argv(tree);
-	tmp = ft_strjoin("/bin/", argv[0]);
-	free(argv[0]);
-	argv[0] = tmp;
-	if (is_cmd(argv[0]) == 0)
+	dynarr = NULL;
+	dynarr = dynarrstr_init(dynarr);
+	dynarrstr_add(dynarr, tree->value);
+	i = 0;
+	while (tree->children[i])
+	{
+		dynarrstr_add(dynarr, tree->children[i]->value);
+		i++;
+	}
+	tmp = ft_strjoin("/bin/", dynarr->array[0]);
+	free(dynarr->array[0]);
+	dynarr->array[0] = tmp;
+	if (is_cmd(dynarr->array[0]) == 0)
 	{
 		printf("Unknow cmd\n");
 		return ;
@@ -38,45 +46,10 @@ void exec(t_tree_node *tree, char **envp)
 		return ;
 	}
 	if (pid == 0)
-		execve(argv[0], argv, envp);
+		execve(dynarr->array[0], dynarr->array, NULL);
 	else
 		wait(NULL);
-}
-
-/*
-	TODO:
-		- Gerer les erreurs de malloc
-*/
-static char	**tree_to_argv(t_tree_node *tree)
-{
-	char	**resp;
-	int		nbr_node;
-	int		i;
-
-	resp = NULL;
-	nbr_node = tree_count_node(tree);
-	if (nbr_node < 1)
-		return (NULL);
-	resp = malloc(sizeof(char *) * (nbr_node + 1));
-	if (resp == NULL)
-		return (NULL);
-
-	i = 0;
-	resp[i] = malloc(sizeof(char) * ft_strlen(tree->value) + 1);
-	resp[i] = ft_strdup(tree->value);
-	i++;
-	if (nbr_node > 1)
-	{
-		while (i < nbr_node)
-		{
-			resp[i] = malloc(sizeof(char) * (ft_strlen(tree->children[i - 1]->value) + 1));
-			resp[i] = ft_strdup(tree->children[i - 1]->value);
-			i++;
-		}
-	}
-	resp[i] = NULL;
-
-	return (resp);
+	dynarrstr_free(dynarr);
 }
 
 static int	is_cmd(char *path)
--- main.c
+++ main.c
@@ -6,16 +6,15 @@
 /*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/06 19:08:50 by lray              #+#    #+#             */
-/*   Updated: 2023/08/11 02:30:27 by lray             ###   ########.fr       */
+/*   Updated: 2023/08/11 19:39:23 by lray             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char **argv, char **envp)
+
+int	main(void)
 {
-	(void)		argc;
-	(void)		argv;
 	char		*input;
 	t_tklist	*tklist;
 	t_tree_node	*tree;
@@ -27,8 +26,23 @@ int	main(int argc, char **argv, char **envp)
 			continue ;
 		tklist = lexer(input);
 		tree = parser(tklist);
-		exec(tree, envp);
+		exec(tree);
 	}
 	clear_history();
 	return (0);
 }
+
+/* int main(void)
+{
+	t_dynarrstr	*dynarr;
+
+	dynarr = NULL;
+	dynarr = dynarrstr_init(dynarr);
+	dynarrstr_add(dynarr, "ls");
+	dynarrstr_add(dynarr, "-l");
+	dynarrstr_add(dynarr, "-a");
+	dynarrstr_add(dynarr, "-t");
+	dynarrstr_show(dynarr);
+	dynarrstr_free(dynarr);
+	return (0);
+} */
