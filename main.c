def465c3e090907e36a856bb4905e926661e1ba2
Luca Ray <luca.ray@protonmail.ch>
2023-08-13 21:59:08 +0200

    adapt main to new parser

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/06 19:08:50 by lray              #+#    #+#             */
-/*   Updated: 2023/08/13 21:21:52 by lray             ###   ########.fr       */
+/*   Updated: 2023/08/13 21:57:17 by lray             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,24 +54,3 @@ static void	free_line(char *input, t_dyntklist *tklist, t_dyntree *tree)
 	dyntree_free(tree);
 	tree = NULL;
 }
-
-/* int	main(void)
-{
-	t_dyntree	*root;
-
-	root = NULL;
-	root = dyntree_new(TK_PIPE, "|");
-	dyntree_add(root, dyntree_new(TK_COMMAND, "grep"));
-	dyntree_add(root->children[0], dyntree_new(TK_REDIRECTION, ">"));
-	dyntree_add(root->children[0]->children[0], dyntree_new(TK_FILE, "infile.txt"));
-	dyntree_add(root->children[0], dyntree_new(TK_ARGUMENT, "test"));
-	dyntree_add(root, dyntree_new(TK_COMMAND, "sort"));
-	dyntree_add(root, dyntree_new(TK_COMMAND, "wc"));
-	dyntree_add(root->children[2], dyntree_new(TK_ARGUMENT, "-l"));
-	dyntree_add(root->children[2], dyntree_new(TK_REDIRECTION, ">"));
-	dyntree_add(root->children[2]->children[1], dyntree_new(TK_FILE, "outfile.txt"));
-	db_show_tree(root, 0);
-	dyntree_free(root);
-	return (0);
-} */
-
