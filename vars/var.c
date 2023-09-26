6b45e355c12cca4189a1b6d91a2e6b6c03979ccc
Luca Ray <luca.ray@protonmail.ch>
2023-09-26 16:08:57 +0200

    implémentation of t_var struct

--- vars/var.c
+++ vars/var.c
@@ -6,9 +6,58 @@
 /*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 15:39:08 by lray              #+#    #+#             */
-/*   Updated: 2023/09/26 15:42:10 by lray             ###   ########.fr       */
+/*   Updated: 2023/09/26 16:02:47 by lray             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+t_var	*var_new(char *name, char *value)
+{
+	t_var	*new;
+
+	if (!name || !value)
+	{
+		ft_puterror("Cannot create var without a name or a value");
+		return (NULL);
+	}
+	new = malloc(sizeof(t_var) * 1);
+	if (!new)
+	{
+		ft_puterror("Malloc failed");
+		return (NULL);
+	}
+	new->name = ft_strdup(name);
+	new->value = ft_strdup(value);
+	return (new);
+}
+
+void	var_show(t_var *var)
+{
+	if (var)
+	{
+		printf("var name : ");
+		if (var->name)
+			printf("%s", var->name);
+		else
+			printf("(null)");
+		printf(" | value : ");
+		if (var->value)
+			printf("%s", var->value);
+		else
+			printf("(null)");
+	}
+	printf("\n");
+}
+
+void	var_free(t_var	*var)
+{
+	if (var)
+	{
+		if (var->name)
+			free(var->name);
+		if (var->value)
+			free(var->value);
+		free(var);
+	}
+}
