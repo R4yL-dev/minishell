c3034f4f74cef406e9b3ac4e54a161dac7a57869
Luca Ray <luca.ray@protonmail.ch>
2023-10-02 16:22:12 +0200

    fix #25

--- expand/replace_builtins.h
+++ expand/replace_builtins.h
@@ -6,12 +6,16 @@
 /*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 15:16:32 by lray              #+#    #+#             */
-/*   Updated: 2023/10/02 15:16:58 by lray             ###   ########.fr       */
+/*   Updated: 2023/10/02 15:45:55 by lray             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef REPLACE_BUILTINS_H
 # define REPLACE_BUILTINS_H
 
+# include <sys/types.h>
+# include <dirent.h>
+
+int	replace_builtins(t_dyntree *root);
 
 #endif
