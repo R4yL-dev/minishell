c896b15a4e2d2308d2c7a676ab05663e5abb9853
Luca Ray <luca.ray@protonmail.ch>
2023-11-01 16:49:53 +0100

    fix exit

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 18:12:24 by mflury            #+#    #+#             */
-/*   Updated: 2023/10/31 20:30:35 by lray             ###   ########.fr       */
+/*   Updated: 2023/11/01 16:44:13 by lray             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,17 @@ int	builtin_exit(char **argv, t_ctx *ctx)
 	int	code;
 
 	(void) argv;
-	code = ctx->ret_code;
+	printf("exit\n");
+	if (!argv || !argv[1])
+		code = ctx->ret_code;
+	else if (argv[2] != NULL)
+	{
+		ft_puterror("exit: too many arguments");
+		return (1);
+	}
+	else
+		code = ft_atoi(argv[1]);
 	clear_history();
 	ctx_free(ctx);
-	printf("exit\n");
 	exit(code);
 }
